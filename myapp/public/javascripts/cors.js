4b4d8d974dbd595e99df1b6e7ce9abeac5ee5c95
Csaba_Bornemissza <bornem2007@yahoo.co.uk>
2014-06-02 11:52:42 +0200

    registration form now works

---
+++ myapp/public/javascripts/cors.js
@@ -0,0 +1,9 @@
+
+//CORS middleware
+exports.allowCrossDomain = function(req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+}
+
